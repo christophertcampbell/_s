2e4d86b7137e3ef3dd304b79de2fe84a87a42262
christophertcampbell <chris@christophertcampbell.com>
2019-03-03 13:31:58 -0800

    Clean up heirarchy in header layout styling

--- header.php
+++ header.php
@@ -25,38 +25,40 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', '_s' ); ?></a>
 
 	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<div class="inner">
-				<?php
-				the_custom_logo();
-				if ( is_front_page() && is_home() ) :
-					?>
-					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+		<div class="inner">
+			<div class="site-branding">
+				<div class="inner">
 					<?php
-				else :
-					?>
-					<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-					<?php
-				endif;
-				$_s_description = get_bloginfo( 'description', 'display' );
-				if ( $_s_description || is_customize_preview() ) :
-					?>
-					<p class="site-description"><?php echo $_s_description; /* WPCS: xss ok. */ ?></p>
-				<?php endif; ?>
-			</div><!-- .inner -->
-		</div><!-- .site-branding -->
+					the_custom_logo();
+					if ( is_front_page() && is_home() ) :
+						?>
+						<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+						<?php
+					else :
+						?>
+						<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+						<?php
+					endif;
+					$_s_description = get_bloginfo( 'description', 'display' );
+					if ( $_s_description || is_customize_preview() ) :
+						?>
+						<p class="site-description"><?php echo $_s_description; /* WPCS: xss ok. */ ?></p>
+					<?php endif; ?>
+				</div><!-- .inner -->
+			</div><!-- .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation">
-			<div class="inner">
-				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><span class="toggle-content"><?php esc_html_e( 'Primary Menu', '_s' ); ?></span></button>
-				<?php
-				wp_nav_menu( array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				) );
-				?>
-			</div><!-- .inner -->
-		</nav><!-- #site-navigation -->
+			<nav id="site-navigation" class="main-navigation">
+				<div class="inner">
+					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><span class="toggle-content"><?php esc_html_e( 'Primary Menu', '_s' ); ?></span></button>
+					<?php
+					wp_nav_menu( array(
+						'theme_location' => 'menu-1',
+						'menu_id'        => 'primary-menu',
+					) );
+					?>
+				</div><!-- .inner -->
+			</nav><!-- #site-navigation -->
+		</div><!-- .inner -->
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
