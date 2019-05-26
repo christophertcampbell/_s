9e6ed3af65950eac7b12386390ea07b0c28ae849
christophertcampbell <chris@christophertcampbell.com>
2019-05-26 00:42:36 -0700

    Add js modified time as version for cache busting

--- functions.php
+++ functions.php
@@ -144,16 +144,27 @@ add_action( 'widgets_init', '_s_widgets_init' );
 function _s_scripts() {
 	wp_enqueue_style( 'dashicons' );
 	wp_enqueue_style( '_s-style', get_stylesheet_directory_uri() . '/style.css', array(), filemtime(get_stylesheet_directory() . '/style.css') );
-	wp_enqueue_script( '_s-expandable', get_template_directory_uri() . '/lib/expandable/expandable.js', array(), '20151215', true );
-	wp_enqueue_script( '_s-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
-	wp_enqueue_script( '_s-scroll-detection', get_template_directory_uri() . '/js/scroll-detection.js', array(), '20151215', true );
-	wp_enqueue_script( '_s-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	_s_enqueue_script( '_s-expandable', '/lib/expandable/expandable.js' );
+	_s_enqueue_script( '_s-navigation', '/js/navigation.js' );
+	_s_enqueue_script( '_s-scroll-detection', '/js/scroll-detection.js' );
+	_s_enqueue_script( '_s-skip-link-focus-fix', '/js/skip-link-focus-fix.js' );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 }
 add_action( 'wp_enqueue_scripts', '_s_scripts' );
+
+/**
+ * Enqueues a script, using the file-modified time as the version for cache busting
+ *
+ * @param string $handle
+ * @param string $relative_path The path relative to the theme directory, including the leading forward slash
+ * @return void
+ */
+function _s_enqueue_script( $handle, $relative_path ) {
+	wp_enqueue_script( $handle, get_template_directory_uri() . $relative_path, array(), filemtime( get_template_directory() . $relative_path ), true );
+}
 
 /**
  * Implement the Custom Header feature.
